c8bb208ed548a15b71494c67de489bec6f68dfa0
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-08 18:18:06 +0000

    Documenting

--- util/hebrewToEnglishFunction.js
+++ util/hebrewToEnglishFunction.js
@@ -2,6 +2,7 @@ const translate = require('translate-google');
 translate.engine = "google";
 
 const toEnglish = async (ivrit) => {
+    //Takes the hebrew text and translates it to english using the translate-google Module
     if (ivrit) {
         return await translate(ivrit, { from: 'iw', to: 'en' });
     }
