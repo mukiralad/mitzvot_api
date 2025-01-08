f1963ad61071cdd4fe1333adaa357078baa6691c
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-08 17:27:06 +0000

    Removed non-required params

--- util/hebrewToEnglishFunction.js
+++ util/hebrewToEnglishFunction.js
@@ -2,9 +2,9 @@ import translate from "translate";
 translate.engine = "google";
 
 // From = he, to = en (example)
-const toEnglish = async(ivrit, from, to) => {
+const toEnglish = async(ivrit) => {
     if (ivrit){
-        const text = await translate(ivrit, { from: from, to: to });
+        const text = await translate(ivrit, { from: he, to: en });
         return text
     }
     return "error"
