a6919587da83e2ac54b62216091f49a71eb13744
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-09 16:51:38 +0000

    Added gemini secret key + imports.. created sample AI file

---
+++ aiFunction.js
@@ -0,0 +1,21 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Load the .env file
+require('dotenv').config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = ``;
+
+const load = async () => {
+    try {
+        const result = await model.generateContent(prompt);
+        const responseText = await result.response.text();
+        console.log(responseText);
+    } catch (error) {
+        console.error("Error generating content:", error);
+    }
+}
+
+load();
