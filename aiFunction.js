2bd77547cebcb504fc47224a8e6aec349af89496
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-12 02:53:53 +0000

    Un-commented AI code.

--- aiFunction.js
+++ aiFunction.js
@@ -1,20 +1,20 @@
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
-// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-// const prompt = ``;
+const prompt = ``;
 
-// const load = async () => {
-//     try {
-//         const result = await model.generateContent(prompt);
-//         const responseText = await result.response.text();
-//         console.log(responseText);
-//     } catch (error) {
-//         console.error("Error generating content:", error);
-//     }
-// }
+const load = async () => {
+    try {
+        const result = await model.generateContent(prompt);
+        const responseText = await result.response.text();
+        console.log(responseText);
+    } catch (error) {
+        console.error("Error generating content:", error);
+    }
+}
 
-// load();
+load();
