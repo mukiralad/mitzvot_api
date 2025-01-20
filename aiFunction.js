446c789b3784ec257b4b9227785bb4013e7564f2
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-01-20 10:42:49 +0000

    Documenting aiFunctions.js functions

--- aiFunction.js
+++ aiFunction.js
@@ -10,6 +10,7 @@ const genAI = new GoogleGenerativeAI(geminiKey);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const mitzvahSummary = async (prompt) => {
+    //Takes in prompt, and returns the AI response.
     try {
         const result = await model.generateContent(`Hi, your only job is to be an Orthadox official Rabbi who evaluates what I have done today, and returns which mitzot I have completed based on my daily summary. be concise give me a mitzvot count for the day. Consider that some actions may complete multiple mitzvot. Give me the response in a Json format of mitzvotForToday: dataHere, mitzvotCount: dataHere. Here is the summary: ${prompt}`);
         const responseText = await result.response.text();
@@ -21,7 +22,9 @@ const mitzvahSummary = async (prompt) => {
 
 const explainMitzvah = async (id) => {
 
+    //Looking for Mitzvah, using find function, where mitzvah number is equal to ID provided in function as a param.
     const mitzvah = mitzvot.find(m => m.number === id);
+
     try {
         const result = await model.generateContent(`Hi, your only job is to be an Orthadox official Rabbi, and to explain the mitzvah in more detail (keep it concise and return it in as a JSON, with the format mitzvahName and explanation): ${mitzvah.description}`)
         const responseText = await result.response.text();
@@ -33,6 +36,7 @@ const explainMitzvah = async (id) => {
 };
 
 const aiSearch = async (query)=>{
+    //Takes in query, and adds this to our AI prompt, returning response.
     try {
         const result = await model.generateContent(`Hi, your only job is to be an Orthadox official Rabbi and suggest any mitzvot that could be related to the following query (using ${mitzvot} as context. Also, return data in a JSON in the format description and potentialMitzvah): ${query}`)
         const responseText = await result.response.text();
